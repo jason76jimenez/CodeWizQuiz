174e1dfab22f345c03b68b039469c01535a8088f
Jason Jimenez <jasonjimenez@MacBook-Pro.local>
2020-10-17 21:40:50 -0500

    timer installed

--- quiz.js
+++ quiz.js
@@ -1,28 +1,44 @@
-var timerEl = document.getElementById('start');
+var timerEl = document.getElementById("timer");
 var mainEl = document.getElementById('question');
 var startBtn = document.getElementById('start');
+alert("Welcome! FYI, wrong answers deduct 10 seconds from time!")
 
 var message =
   'Congratulations! Now you are prepared to tackle the Challenge this week! Good luck!';
 var words = message.split(' ');
 
-// Timer that counts down from 5
+// Timer
 function start() {
-  var timeLeft = 5;
+  var timeLeft = 300; //this is the timer time parameter
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
-      timerEl.textContent = timeLeft + 'seconds remaining!';
-      if (timeLeft ===1) {
-          timerEl.textContent = timeLeft + 'seconds remaining!'
+      var min= Math.floor(timeLeft/60); //defines the minutes
+      var sec= timeLeft%60;  //defines the seconds
+      if (sec<10) sec='0'+sec  //if sec <10 will display 09 instead of just 9
+      timerEl.textContent = min + ':' + sec +' remaining';
+      if (timeLeft ===10) {
+          timerEl.textContent = timeLeft + ' seconds remaining!'
       }else if (timeLeft <1) {
           timerEl.textContent = ' ';
           clearInterval(timeInterval);
           displayMessage();
       }
-      timeLeft --
-      }, 1000);
+      timeLeft -- //displays the time continuously
+      }, 1000);  //this function read every 1 sec.
     //
+
+function startQuiz() {
+    alert("first question")
+    console.log("debug")
+
+}
+
+
+
+
+
+
+
     // YOUR CODE HERE
     //
 }
@@ -42,4 +58,4 @@ function displayMessage() {
   }, 300);
 }
 
-startBtn.onclick = countdown;
+startBtn.onclick = start; //This is the onclick command
